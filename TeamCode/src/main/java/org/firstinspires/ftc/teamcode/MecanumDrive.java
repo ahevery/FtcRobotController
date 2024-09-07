906981d7186aaa3a176729c74b9136402f7665b6
vihaanreddi <vihaanreddi@gmail.com>
2024-09-07 11:02:18 -0700

    roadrunner works???

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -68,7 +68,7 @@ public final class MecanumDrive {
         // feedforward parameters (in tick units)
         public double kS = 0.8652973688185792;
         public double kV = 0.5689161943080484;
-        public double kA = 0.01;
+        public double kA = 0.000001;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
@@ -80,9 +80,9 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 0.005;
+        public double lateralGain = 0.005;
+        public double headingGain = 0.005; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
